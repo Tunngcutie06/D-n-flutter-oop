8d596a28461b8b1777c4d80d7085422039aca876
Tunngcutie06 <nguyenhuuanhtuan06@gmail.com>
2025-10-27 21:36:52 +0700

    Delete lib directory

--- lib/models/item.dart
+++
@@ -1,9 +0,0 @@
-import 'player.dart';
-
-class Item {
-  final String asset;
-  final void Function(Player) onPickup;
-  double x;
-  double y;
-  Item(this.asset, this.onPickup, {required this.x, required this.y});
-}
